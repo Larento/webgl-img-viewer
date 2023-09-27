059c3299e6491bfa21862acfbf31fb5b5c1f1a34
Иларион Ерченко <turolretar@gmail.com>
2023-09-27 10:18:29 +0300

    :sparkles: add ability to call methods directly on matrix class instance instead of just static methods; use homogenous coordinates for transforms; add new operations

--- src/classes/matrix.ts
+++ src/classes/matrix.ts
@@ -1,30 +1,106 @@
+type MatrixData = number[][];
 export class Matrix {
-    static identity(): number[][] {
-        return [
-            [1, 0, 0],
-            [0, 1, 0],
+    readonly data: MatrixData;
+
+    static zero(rows = 4, columns = rows): Matrix {
+        return new Matrix(Array.from(Array(rows), () => Array.from(Array(columns), () => 0)));
+    }
+
+    static identity(size = 4): Matrix {
+        return new Matrix(
+            Array.from(Array(size), (_, row) =>
+                Array.from(Array(size), (_, column) => (row === column ? 1 : 0)),
+            ),
+        );
+    }
+
+    static translation(xOffset: number, yOffset: number, zOffset: number): Matrix {
+        const mat = Matrix.identity(4);
+        mat.data[0][3] = xOffset;
+        mat.data[1][3] = yOffset;
+        mat.data[2][3] = zOffset;
+        return mat;
+    }
+
+    static scaling(factor: number): Matrix {
+        return Matrix.identity(3).multiplyByScalar(factor).toHomogenous();
+    }
+
+    static rotation(angle: number): Matrix {
+        const { cos, sin } = Math;
+        return new Matrix([
+            [cos(angle), -sin(angle), 0],
+            [sin(angle), cos(angle), 0],
             [0, 0, 1],
-        ];
+        ]).toHomogenous();
     }
 
-    static multiplyByScalar(mat: number[][], scalar: number): number[][] {
-        return mat.map((row) => row.map((element) => element * scalar));
+    static multiplyArray(matArray: Matrix[]): Matrix {
+        const firstMat = matArray[0];
+        return matArray
+            .slice(1)
+            .reduce(
+                (multipliedMatrix, currentMatrix) => multipliedMatrix.multiply(currentMatrix),
+                firstMat,
+            );
     }
 
-    static multiply(matA: number[][], matB: number[][]): number[][] {
-        if (matA.length !== matB[0].length) {
-            throw new RangeError('Matrices are not compatible for multiplication.');
-        }
+    constructor(data: MatrixData) {
+        this.data = data;
+    }
 
-        const columns = matB.length;
-        const rows = matA.length;
-        const sumTermsCount = matA[0].length;
-        const outputMatrix: number[][] = Array.from(Array(rows), () => []);
+    transpose() {
+        const rows = this.data.length;
+        const columns = this.data[0].length;
+        const outputMatrix = Matrix.zero(columns, rows);
 
         for (let i = 0; i < rows; i += 1) {
             for (let j = 0; j < columns; j += 1) {
-                for (let k = 0; k < sumTermsCount; k += 1) {
-                    outputMatrix[i][j] += matA[i][k] * matB[k][j];
+                outputMatrix.data[j][i] = this.data[i][j];
+            }
+        }
+        return outputMatrix;
+    }
+
+    toHomogenous() {
+        return new Matrix([...this.data.map((row) => [...row, 0]), [0, 0, 0, 1]]);
+    }
+
+    add(mat: Matrix) {
+        return new Matrix(
+            this.data.map((row, rowIndex) =>
+                row.map((column, columnIndex) => column + mat.data[rowIndex][columnIndex]),
+            ),
+        );
+    }
+
+    subtract(mat: Matrix) {
+        return this.add(mat.multiplyByScalar(-1));
+    }
+
+    multiplyByScalar(scalar: number): Matrix {
+        return new Matrix(this.data.map((row) => row.map((element) => element * scalar)));
+    }
+
+    multiply(mat: Matrix): Matrix {
+        const matARows = this.data.length;
+        const matAColumns = this.data[0].length;
+
+        const matBRows = mat.data.length;
+        const matBColumns = mat.data[0].length;
+
+        if (matAColumns !== matBRows) {
+            throw new RangeError(
+                `Matrices of sizes '${matARows}x${matAColumns}' and '${matBRows}x${matBColumns}' are not compatible for multiplication.`,
+            );
+        }
+
+        const outputMatrix = Matrix.zero(matARows, matBColumns);
+
+        for (let i = 0; i < matARows; i += 1) {
+            for (let j = 0; j < matBColumns; j += 1) {
+                for (let k = 0; k < matAColumns; k += 1) {
+                    outputMatrix.data[i][j] += this.data[i][k] * mat.data[k][j];
                 }
             }
         }
